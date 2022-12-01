eabec6d8b6fec6da3459982c0c7c6a311d15f71b
Leon Tur <tutrkov.leon-2@yandex.ru>
2022-12-01 22:19:01 +0300

    Finshed add contact finder

---
+++ model/find_contact.py
@@ -0,0 +1,27 @@
+# Модуль осуществляет поиск контакта пл имени или номеру телефона
+
+import codecs
+
+def Find_by_name ():
+    name = input('Введите имя: ')
+    num_line_list =[]
+    with codecs.open('C:/Users/thebe/OneDrive/Рабочий стол/GIT/Python/Python_7/data/phonebook.txt', 'r', 'utf-8') as phonebook:
+        _ = 0
+        for line in phonebook:
+            _ += 1
+            if name in line:
+                num_line_list.append(_)
+    
+    return num_line_list
+
+def Find_by_name ():
+    number = input('Введите имя: ')
+    num_line_list =[]
+    with codecs.open('C:/Users/thebe/OneDrive/Рабочий стол/GIT/Python/Python_7/data/phonebook.txt', 'r', 'utf-8') as phonebook:
+        _ = 0
+        for line in phonebook:
+            _ += 1
+            if number in line:
+                num_line_list.append(_)
+    
+    return num_line_list
